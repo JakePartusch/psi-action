21412db1e3a844ccb9a42324f0d59177bc7a6d07
Jacob Partusch <jakepartusch@gmail.com>
2020-01-07 08:45:19 -0600

    feat: using configured url to produce output

--- index.js
+++ index.js
@@ -3,26 +3,9 @@ const psi = require("psi");
 
 const run = async () => {
   try {
-    // Get the PageSpeed Insights report
-    const { data } = await psi("https://theverge.com");
-    console.log(
-      "Speed score:",
-      data.lighthouseResult.categories.performance.score
-    );
-
+    const url = core.getInput("url");
     // Output a formatted report to the terminal
-    await psi.output("https://theverge.com");
-    console.log("Done");
-
-    // Supply options to PSI and get back speed
-    const data2 = await psi("https://theverge.com", {
-      nokey: "true",
-      strategy: "desktop"
-    });
-    console.log(
-      "Speed score:",
-      data2.data.lighthouseResult.categories.performance.score
-    );
+    await psi.output(url);
   } catch (error) {
     core.setFailed(error.message);
   }
