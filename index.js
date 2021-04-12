62436cfd4132e5e338dec1147d35bdb1058ef274
Simon Jang <simon.jang.contact@gmail.com>
2021-04-12 03:41:20 +0200

    feat: adding `key` param for psi API key - #5 (#6)

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ const run = async () => {
       core.setFailed("Url is required to run Page Speed Insights.");
       return;
     }
+
+    const key = core.getInput('key');
+
     const threshold = Number(core.getInput("threshold")) || 70;
     const strategy = core.getInput("strategy") || "mobile";
     // Output a formatted report to the terminal
     console.log(`Running Page Speed Insights for ${url}`);
     await psi.output(url, {
-      nokey: "true",
+      ...(key ? {key} : undefined),
+      ...(key ? undefined : {nokey: "true"}),
       strategy,
       format: "cli",
       threshold
