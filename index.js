40884f4823e36afd50dd732ea481d498f137bc8f
emc-kk <emc.kk0314@gmail.com>
2022-07-15 02:17:10 +0900

    Adding parameters to comment the PSI results once Pull Request occurred

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const core = require("@actions/core");
 const psi = require("psi");
 
+const coreWebVitalsTypes = ["FCP", "TTI", "SI", "TBT", "LCP", "CLS", "FMP"];
+
 const run = async () => {
   try {
     const url = core.getInput("url");
@@ -13,18 +15,42 @@ const run = async () => {
 
     const threshold = Number(core.getInput("threshold")) || 70;
     const strategy = core.getInput("strategy") || "mobile";
-    // Output a formatted report to the terminal
     console.log(`Running Page Speed Insights for ${url}`);
-    await psi.output(url, {
+
+    const params = {
       ...(key ? {key} : undefined),
       ...(key ? undefined : {nokey: "true"}),
       strategy,
       format: "cli",
       threshold
-    });
+    };
+
+    // Output a formatted report to the terminal
+    await psi.output(url, params);
+
+    if (activity_type === "pull_request") {
+      // Output a simple report to the pull request comment section
+      const output = await psi(url, params);
+      const performance = output.data.lighthouseResult.categories.performance;
+      core.setOutput("score", JSON.stringify(performance.score));
+      const coreWebVitals = getCoreWebVitals(performance.auditRefs);
+      setCoreWebVitals(coreWebVitals);
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
 };
+
+const getCoreWebVitals = (refs) => {
+  return refs.reduce((acc, ref, _) => {
+    if (coreWebVitalsTypes.includes(ref.acronym))
+      acc[ref.acronym] = ref.weight;
+  }, {});
+}
+
+const setCoreWebVitals = (coreWebVitals) => {
+  for (const type of coreWebVitalsTypes) 
+      core.setOutput(type.toLowerCase(), JSON.stringify(coreWebVitals[type]));
+}
 
 run();
